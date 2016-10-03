01a70bc38f607c43afe9ce5ea12322573dece9d9
Roman <haydey.roman@gmail.com>
2016-10-03 09:52:27 +0300

    Second Commit

--- Dikhotomia.m
+++ Dikhotomia.m
@@ -4,4 +4,27 @@ function y=f(x)
 y=(x+1).^2;
 end
 
+function y=D(a,b,eps)
+x=[];
+i=1;
+if (round(abs(b-a)*10000)/10000>eps)
+    while(round(abs(b-a)*10000)/10000>eps)
+        t=0.5*(a+b-eps);
+        z=0.5*(a+b+eps);
+        ft=f(t);
+        fz=f(z);
+        if(ft<=fz)
+            b=z;
+        else
+            a=t;
+        x(i)=0.5*(a+b);
+        i=i+1;
+        end
+    end
+else
+    x(i)=0.5*(a+b);
+end
+y=x;
+end
+
 end
